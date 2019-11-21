ad922d88575441f3af55be1e83b54a15726e0ef4
shehan <shehanhasanga94@gmail.com>
2019-11-21 15:15:58 +0530

    second_commit

--- ts.js
+++ ts.js
@@ -1,15 +1,13 @@
 var serialize = require('node-serialize');
 console.log('shehan');
 var obj = {
-    name: 'Bob',
-    say: function() {
-      return 'hi ' + this.name;
-    }
-  };
-
-  obj.name="sheha";
-  
-  var objS = serialize.serialize(obj);
-  console.log(objS);
-  typeof objS === 'string';
+  name: 'Bob',
+  say: function () {
+    return 'hi ' + this.name;
+  }
+};
+obj.name = "Alice";
+var objS = serialize.serialize(obj);
+console.log(objS);
+typeof objS === 'string';
 serialize.unserialize(objS).say() === 'hi Bob';
--- ts1.js
+++ ts1.js
@@ -1,4 +1,3 @@
-
 var serialize = require('node-serialize');
 var objS = '{"name":"sheha","say":"_$$ND_FUNC$$_function() {return \'hi \' + this.name;}"}'
 console.log(serialize.unserialize(objS));
